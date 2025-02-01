738730d2f3050ebd905eb939d4da01d0f758ab12
nwaxon2020 <princenwachukwu308@yahoo.com>
2025-02-01 13:41:28 +0100

    good

--- nextjs-dashboard/auth.config.ts
+++ nextjs-dashboard/auth.config.ts
@@ -1,4 +1,5 @@
 import type { NextAuthConfig } from 'next-auth';
+import GitHub from 'next-auth/providers/github';
  
 export const authConfig = {
   pages: {
@@ -17,5 +18,10 @@ export const authConfig = {
       return true;
     },
   },
-  providers: [], // Add providers with an empty array for now
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+  ], // Add providers with an empty array for now
 } satisfies NextAuthConfig;
