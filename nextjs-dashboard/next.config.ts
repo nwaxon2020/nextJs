6fad9d2b63d4fe513f42b95d2fd189df8a66e7c4
nwaxon2020 <princenwachukwu308@yahoo.com>
2025-02-01 12:59:41 +0100

    newe

--- nextjs-dashboard/next.config.ts
+++ nextjs-dashboard/next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
-module.exports = nextConfig;
